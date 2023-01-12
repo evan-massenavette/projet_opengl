e580bc8775b7a10e607f9807c1eb7ff70f48745b
Evan MASSENAVETTE <evan.massenavette@outlook.fr>
2023-01-12 19:33:38 +0100

    texture properties

--- src/texture.cpp
+++ src/texture.cpp
@@ -24,17 +24,21 @@ GLuint Texture::load(const char *filepath) {
                textureImage.height(), 0, GL_BGR, GL_UNSIGNED_BYTE,
                textureImage.data());
 
-  // Poor filtering, or ...
+  // Texture properties
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+  // Poor filtering
   // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
-  // ... nice trilinear filtering ...
+  // Nice trilinear filtering
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
                   GL_LINEAR_MIPMAP_LINEAR);
-  // ... which requires mipmaps. Generate them automatically.
+  // Generate mipmaps automatically
   glGenerateMipmap(GL_TEXTURE_2D);
 
   // Return the ID of the texture we just created
