03a4d7d84dd4c249d178b1b3a2c8c66f799c2b50
Evan MASSENAVETTE <evan.massenavette@outlook.fr>
2023-01-18 18:17:37 +0100

    Remove old buffer data + old vertex classes

--- src/vertex.cpp
+++ src/vertex.cpp
@@ -2,15 +2,3 @@
 
 Vertex::Vertex(glm::vec3 position, glm::vec3 normal, glm::vec2 uv)
     : position(position), normal(normal), uv(uv) {}
-
-Vertex2dColor::Vertex2dColor(glm::vec2 position, glm::vec3 color)
-    : position(position), color(color) {}
-
-Vertex2dUV::Vertex2dUV(glm::vec2 position, glm::vec2 uv)
-    : position(position), uv(uv) {}
-
-Vertex3dColor::Vertex3dColor(glm::vec3 position, glm::vec3 color)
-    : position(position), color(color) {}
-
-Vertex3dUV::Vertex3dUV(glm::vec3 position, glm::vec2 uv)
-    : position(position), uv(uv) {}
--- src/vertex.hpp
+++ src/vertex.hpp
@@ -11,32 +11,4 @@ public:
   Vertex(glm::vec3 position, glm::vec3 normal, glm::vec2 uv);
 };
 
-class Vertex2dColor {
-public:
-  glm::vec2 position;
-  glm::vec3 color;
-  Vertex2dColor(glm::vec2 position, glm::vec3 color);
-};
-
-class Vertex2dUV {
-public:
-  glm::vec2 position;
-  glm::vec2 uv;
-  Vertex2dUV(glm::vec2 position, glm::vec2 uv);
-};
-
-class Vertex3dColor {
-public:
-  const glm::vec3 position;
-  const glm::vec3 color;
-  Vertex3dColor(glm::vec3 position, glm::vec3 color);
-};
-
-class Vertex3dUV {
-public:
-  glm::vec3 position;
-  glm::vec2 uv;
-  Vertex3dUV(glm::vec3 position, glm::vec2 uv);
-};
-
 #endif
