96aac66ccf6ad4b43c964d1e19de1e93d7ae03b1
Evan MASSENAVETTE <evan.massenavette@outlook.fr>
2023-01-20 19:59:09 +0100

    Added Ambient and Point lights to Scene

--- src/scene/scene.hpp
+++ src/scene/scene.hpp
@@ -6,7 +6,10 @@
 
 #include <glm/glm.hpp>
 
+#include "../shader_structs/ambient_light.hpp"
 #include "../shader_structs/directional_light.hpp"
+#include "../shader_structs/material.hpp"
+#include "../shader_structs/point_light.hpp"
 
 #include "globject.hpp"
 
@@ -17,9 +20,17 @@ class Scene {
   // Objects
   std::vector<std::unique_ptr<GLObject>> objects;
 
+  // Ambient lights
+  static const size_t MAX_AMBIENT_LIGHTS = 8;  // Same as in shaders
+  std::vector<shader_structs::AmbientLight> ambientLights;
+
   // Directional lights
-  static const size_t MAX_DIRECTIONAL_LIGHTS = 8;
+  static const size_t MAX_DIRECTIONAL_LIGHTS = 8;  // Same as in shaders
   std::vector<shader_structs::DirectionalLight> directionalLights;
+
+  // Point lights
+  static const size_t MAX_POINT_LIGHTS = 8;  // Same as in shaders
+  std::vector<shader_structs::PointLight> pointLights;
 
   Scene(const glm::vec4& backgroundColor = glm::vec4(0),
         const bool isDefault = false);
