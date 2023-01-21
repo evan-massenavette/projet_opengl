3ad5b1e64c08b24765078622451415f75d03a30d
Evan MASSENAVETTE <evan.massenavette@outlook.fr>
2023-01-21 16:44:53 +0100

    Revamped App class + using FlyingCamera

--- src/app.cpp
+++ src/app.cpp
@@ -6,82 +6,247 @@
 
 #include "app.hpp"
 #include "controls.hpp"
+#include "flying_camera.hpp"
 #include "renderer.hpp"
 #include "scene/scene.hpp"
 #include "texture.hpp"
 
-void App::_processResize(GLFWwindow* window, int width, int height) {
-  glViewport(0, 0, width, height);
-  printf("Window resized to %d x %d\n", width, height);
+App::App() {
+  // Set all keys as not pressed
+  for (auto& kwp : _keyWasPressed) {
+    kwp = false;
+  }
 }
 
-void App::_openWindow() {
+bool App::createWindow(const std::string& windowTitle,
+                       int majorVersion,
+                       int minorVersion,
+                       bool showFullscreen) {
   // Initialize and configure GLFW
   if (!glfwInit()) {
     std::cerr << "Failed to initialize GLFW\n";
-    getchar();
-    exit(EXIT_FAILURE);
+    return false;
   }
-  glfwWindowHint(GLFW_SAMPLES, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, majorVersion);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minorVersion);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_SAMPLES, 4);
 
-  // Create window
-  _window = glfwCreateWindow(1024, 768, "HGL", NULL, NULL);
-  if (!_window) {
+  const auto primaryMonitor = glfwGetPrimaryMonitor();
+  const auto videoMode = glfwGetVideoMode(primaryMonitor);
+
+  // Default window size is half of screen's resolution
+  // (but no smaller than minimum size)
+  int defaultWidth = std::max(minScreenWidth_, videoMode->width / 2);
+  int defaultHeight = std::max(minScreenHeight_, videoMode->height / 2);
+
+  // Create the window
+  _window =
+      glfwCreateWindow(defaultWidth, defaultHeight, windowTitle.c_str(),
+                       showFullscreen ? primaryMonitor : nullptr, nullptr);
+  if (_window == nullptr) {
     std::cerr << "Failed to create GLFW window\n";
-    getchar();
-    glfwTerminate();
-    exit(EXIT_FAILURE);
+    return false;
   }
   glfwMakeContextCurrent(_window);
-  glfwSetFramebufferSizeCallback(_window, _processResize);
+
+  // Set the minimum window size
+  glfwSetWindowSizeLimits(_window, minScreenWidth_, minScreenHeight_,
+                          GLFW_DONT_CARE, GLFW_DONT_CARE);
+
+  // Set app pointer associated with this window
+  glfwSetWindowUserPointer(_window, this);
+
+  // Set window's static callbacks
+  glfwSetWindowSizeCallback(_window, onWindowSizeChangedStatic);
 
   // Ensure we can capture the escape key being pressed below
   glfwSetInputMode(_window, GLFW_STICKY_KEYS, GL_TRUE);
   // Hide the mouse and enable unlimited mouvement
   glfwSetInputMode(_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-  // Initialize GLAD
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+  // Load GLAD
+  if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
     std::cerr << "Failed to initialize GLAD\n";
-    getchar();
-    exit(EXIT_FAILURE);
+    return false;
   }
+
+  return true;
 }
 
-void App::_closeWindow() {
-  // Close OpenGL window and terminate GLFW
+void App::destroyWindow() {
   glfwDestroyWindow(_window);
   glfwTerminate();
 }
 
+GLFWwindow* App::getWindow() const {
+  return _window;
+}
+
 void App::run() {
-  // Open the application window
-  _openWindow();
+  // Open window
+  createWindow("Projet OpenGL Evan & Vincent", 3, 3, false);
 
-  Controls controls;
-  Scene scene(glm::vec4(0.0, 0.0, 0.2, 0.0), true);
+  // Init
+  setVerticalSynchronization(true);
+  recalculateProjectionMatrix();
+
+  // Update time at the beginning, so that calculations are correct
+  _lastFrameTime = _lastFrameTimeFPS = glfwGetTime();
+
+  // Objects used during main loop
+  Scene scene(glm::vec4(0.0, 0.0, 0.2, 1.0), true);
+  FlyingCamera camera(glm::vec3(0, 0, 0), glm::vec3(1, 0, 0),
+                      glm::vec3(0, 1, 0));
   Renderer renderer(scene);
+  Controls controls;
 
-  // Main loop
-  while (!glfwWindowShouldClose(_window)) {
+  while (glfwWindowShouldClose(_window) == 0) {
+    updateDeltaTimeAndFPS();
+
+    // Render
+    renderer.update(getProjectionMatrix(), camera.getViewMatrix());
+
+    // Draw to screen + poll events
+    glfwSwapBuffers(_window);
+    glfwPollEvents();
+
     // Process inputs
     controls.processInputs(_window);
 
-    // Matrices from the controls
-    glm::mat4 projection = controls.getProjectionMatrix();
-    glm::mat4 view = controls.getViewMatrix();
+    // Update camera
+    auto keyInputFunc = [this](int keyCode) {
+      return this->keyPressed(keyCode);
+    };
+    auto getCursorPosFunc = [this]() {
+      double curPosX, curPosY;
+      glfwGetCursorPos(this->_window, &curPosX, &curPosY);
+      return glm::u32vec2(curPosX, curPosY);
+    };
+    auto setCursorPosFunc = [this](const glm::i32vec2& pos) {
+      glfwSetCursorPos(this->_window, pos.x, pos.y);
+    };
+    auto speedCorrectionFunc = [this](float f) { return this->sof(f); };
+    camera.update(keyInputFunc, getCursorPosFunc, setCursorPosFunc,
+                  speedCorrectionFunc);
 
-    // Update the render
-    renderer.update(projection, view);
-
-    // Swap buffers
-    glfwSwapBuffers(_window);
-    glfwPollEvents();
+    // Update scene
   }
 
-  _closeWindow();
+  destroyWindow();
+}
+
+bool App::keyPressed(int keyCode) const {
+  return glfwGetKey(_window, keyCode) == GLFW_PRESS;
+}
+
+bool App::keyPressedOnce(int keyCode) {
+  bool result = false;
+  if (keyPressed(keyCode)) {
+    if (!_keyWasPressed[keyCode]) {
+      result = true;
+    }
+
+    _keyWasPressed[keyCode] = true;
+  } else {
+    _keyWasPressed[keyCode] = false;
+  }
+
+  return result;
+}
+
+void App::closeWindow(bool hasErrorOccurred) {
+  glfwSetWindowShouldClose(_window, true);
+  hasErrorOccurred_ = hasErrorOccurred;
+}
+
+bool App::hasErrorOccurred() const {
+  return hasErrorOccurred_;
+}
+
+glm::mat4 App::getProjectionMatrix() const {
+  return _projectionMatrix;
+}
+
+float App::sof(float value) const {
+  return value * static_cast<float>(_timeDelta);
+}
+
+double App::getTimeDelta() const {
+  return _timeDelta;
+}
+
+int App::getFPS() const {
+  return _FPS;
+}
+
+void App::setVerticalSynchronization(bool enable) {
+  glfwSwapInterval(enable ? 1 : 0);
+  _isVerticalSynchronizationEnabled = enable;
+}
+
+bool App::isVerticalSynchronizationEnabled() const {
+  return _isVerticalSynchronizationEnabled;
+}
+
+int App::getScreenWidth() const {
+  return screenWidth_;
+}
+
+int App::getScreenHeight() const {
+  return screenHeight_;
+}
+
+glm::ivec2 App::getCursorPosition() const {
+  double posX, posY;
+  glfwGetCursorPos(_window, &posX, &posY);
+  return glm::ivec2(static_cast<int>(posX),
+                    screenHeight_ - static_cast<int>(posY));
+}
+
+void App::recalculateProjectionMatrix() {
+  // App width, height and aspect ratio
+  int width, height;
+  glfwGetWindowSize(_window, &width, &height);
+  float aspectRatio = static_cast<float>(width) / static_cast<float>(height);
+
+  // Other vars used for projection
+  float vFov = 60.0f;  // Vertical fov in degrees
+  float zNear = 0.1f;
+  float zFar = 1500.0f;
+
+  // Projection matrix calculation
+  _projectionMatrix =
+      glm::perspective(glm::radians(vFov), aspectRatio, zNear, zFar);
+}
+
+void App::updateDeltaTimeAndFPS() {
+  const auto currentTime = glfwGetTime();
+  _timeDelta = currentTime - _lastFrameTime;
+  _lastFrameTime = currentTime;
+  _nextFPS++;
+
+  if (currentTime - _lastFrameTimeFPS > 1.0) {
+    _lastFrameTimeFPS = currentTime;
+    _FPS = _nextFPS;
+    _nextFPS = 0;
+  }
+}
+
+void App::onWindowSizeChangedInternal(int width, int height) {
+  recalculateProjectionMatrix();
+  glViewport(0, 0, width, height);
+  screenWidth_ = width;
+  screenHeight_ = height;
+}
+
+App* App::getAppPtrFromWindow(GLFWwindow* window) {
+  auto app = reinterpret_cast<App*>(glfwGetWindowUserPointer(window));
+  return app;
+}
+
+void App::onWindowSizeChangedStatic(GLFWwindow* window, int width, int height) {
+  auto app = getAppPtrFromWindow(window);
+  app->onWindowSizeChangedInternal(width, height);
 }
--- src/app.hpp
+++ src/app.hpp
@@ -1,21 +1,198 @@
 #ifndef APP_HPP
 #define APP_HPP
 
+#include <string>
+
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
+#include <glm/glm.hpp>
+
 class App {
-public:
+ public:
+  /**
+   * Constructor of the class, initializes internal structures.
+   */
+  App();
+
+  /**
+   * Creates a window with OpenGL context with given title and context version.
+   *
+   *  @param windowTitle     Title of the created window
+   *  @param majorVersion    OpenGL context major version
+   *  @param minorVersion    OpenGL context minor version
+   *  @param showFullscreen  Tells, whether the window should be shown in
+   * fullscreen
+   *
+   *  @return True if window has been created successfully or false otherwise.
+   */
+  bool createWindow(const std::string& windowTitle,
+                    int majorVersion,
+                    int minorVersion,
+                    bool showFullscreen);
+
+  void destroyWindow();
+
+  /**
+   * Creates a window with OpenGL context with given title and context version.
+   *
+   * @param windowTitle  Title of a created window
+   *
+   * @return Pointer to GLFW window or nullptr, if the window does not exist
+   * yet.
+   */
+  GLFWwindow* getWindow() const;
+
+  /**
+   * Runs the whole application. Contains the main application loop.
+   */
   void run();
 
-private:
-  GLFWwindow *_window;
+  /**
+   * Checks, if specified key is currently pressed.
+   *
+   * @param keyCode  GLFW code of the key to check
+   *
+   * @return True if key is pressed or false otherwise.
+   */
+  bool keyPressed(int keyCode) const;
 
-  void _openWindow();
-  void _closeWindow();
+  /**
+   * Checks, if specified key was pressed. This function won't return true
+   * again, unless the key has been released and pressed again.
+   *
+   * @param keyCode  GLFW code of the key to check
+   *
+   * @return True, if key was pressed once, or false otherwise.
+   */
+  bool keyPressedOnce(int keyCode);
 
-  static void _processResize(GLFWwindow *window, int width, int height);
+  /**
+   * Closes application window and releases all resources. Also sets the error
+   * flag, if error has occured.
+   *
+   * @param errorHasOccured  Should be true, if an error has occured (default is
+   * false).
+   */
+  void closeWindow(bool errorHasOccured = false);
+
+  /**
+   * Gets the error flag.
+   */
+  bool hasErrorOccurred() const;
+
+  /**
+   * Gets the projection matrix for the current window size.
+   */
+  glm::mat4 getProjectionMatrix() const;
+
+  /**
+   * Gets adjusted float value, that takes frames per second into account.
+   *
+   * @param value  Float value to be adjusted
+   *
+   * @return "Speed Optimized Floating Point Value (sof)".
+   */
+  float sof(float value) const;
+
+  /**
+   * Gets time delta - time passed since the last frame in seconds.
+   */
+  double getTimeDelta() const;
+
+  /**
+   * Gets current Frames Per Second (FPS).
+   */
+  int getFPS() const;
+
+  /**
+   * Turns vertical synchronization on or off.
+   *
+   * @param enable  True if you want to enable vertical synchronization or false
+   * otherwise
+   */
+  void setVerticalSynchronization(bool enable);
+
+  /**
+   * Checks, if vertical synchronization is on or off.
+   *
+   * @return True, if vertical synchronization is enabled, or false otherwise.
+   */
+  bool isVerticalSynchronizationEnabled() const;
+
+  /**
+   * Gets current screen width in pixels.
+   */
+  int getScreenWidth() const;
+
+  /**
+   * Gets current screen height in pixels.
+   */
+  int getScreenHeight() const;
+
+  /**
+   * Gets cursor position in OpenGL coordinates.
+   */
+  glm::ivec2 getCursorPosition() const;
+
+ private:
+  GLFWwindow* _window = nullptr;  // Pointer to GLFWwindow, nullptr by default
+  bool _keyWasPressed[512];  // Array of bools, used by keyPressedOnce function
+  bool hasErrorOccurred_ =
+      false;  // Error flag, indicates, if any kind of error has occured
+
+  glm::mat4 _projectionMatrix;  // Precalculated projection matrix, when size
+                                // changes, it's recalculated
+
+  double _lastFrameTime = 0.0;     // Time of last frame
+  double _lastFrameTimeFPS = 0.0;  // Time of last FPS Update
+  double _timeDelta = 0.0;  // Time delta between last frame and current frame
+  int _FPS = 0;             // Current FPS
+  int _nextFPS = 0;  // Next FPS, that is being counted and will update the _FPS
+  bool _isVerticalSynchronizationEnabled =
+      false;  // Stores information, if vertical synchronization is enabled
+
+  bool _hasMouseBeenScrolled = false;
+  double _scrollOffsetX = 0.0;
+  double _scrollOffsetY = 0.0;
+
+  int minScreenWidth_ = 800;   // Minimum screen width
+  int minScreenHeight_ = 600;  // Minimum screen height
+  int screenWidth_ = 0;        // Cached screen width
+  int screenHeight_ = 0;       // Cached screen height
+
+  /**
+   * Recalculates the app's projection matrix
+   */
+  void recalculateProjectionMatrix();
+
+  /**
+   * Updates frame times and FPS counters.
+   */
+  void updateDeltaTimeAndFPS();
+
+  /**
+   * Gets called when window size is changed. Does some internal stuff
+   * (like recalculating matrices, setting viewport) and calls user's handler as
+   * well.
+   */
+  void onWindowSizeChangedInternal(int width, int height);
+
+  /**
+   * Get the pointer to the app associated with a GLFW window
+   * @param window Pointer to the GLFW Window
+   * @return Pointer to the App associated with the window
+   */
+  static App* getAppPtrFromWindow(GLFWwindow* window);
+
+  /**
+   * Static method that is set as callback to GLFW framework about window size
+   * changed.
+   */
+  static void onWindowSizeChangedStatic(GLFWwindow* window,
+                                        int width,
+                                        int height);
 };
 
 #endif
