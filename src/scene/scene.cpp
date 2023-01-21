4378d996a7b04ffe855a3d78ef618679db243d44
Evan MASSENAVETTE <evan.massenavette@outlook.fr>
2023-01-21 23:11:14 +0100

    Change scene lights

--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -21,11 +21,11 @@ void Scene::_initDefaultScene() {
 
   // Directional lights
   shader_structs::DirectionalLight directionalLight1(glm::vec3(1, 1, 1),
-                                                     glm::vec3(1, 0, 0), 1);
+                                                     glm::vec3(0, 1, 0), 1);
   // directionalLights.emplace_back(directionalLight1);
 
   // Point lights
-  shader_structs::PointLight pointLight1(glm::vec3(0, 0, 0), glm::vec3(1, 0, 0),
-                                         1);
+  shader_structs::PointLight pointLight1(glm::vec3(1.5, 1.5, 1.5),
+                                         glm::vec3(1, 0, 0), 1);
   pointLights.emplace_back(pointLight1);
 }
