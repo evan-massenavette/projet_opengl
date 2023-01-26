82b39051478074c737ecbf27aba8fb056d31c422
2K-d <rouxv894@gmail.com>
2023-01-26 17:36:11 +0100

    premier model rollercoaster

--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -3,32 +3,31 @@
 
 #include "scene.hpp"
 
-Scene::Scene(const glm::vec4& backgroundColor, const bool isDefault)
-    : backgroundColor(backgroundColor) {
+Scene::Scene(const glm::vec4 &backgroundColor, const bool isDefault)
+    : backgroundColor(backgroundColor)
+{
   if (isDefault)
     _initDefaultScene();
 }
 
-void Scene::_initDefaultScene() {
+void Scene::_initDefaultScene()
+{
   // Objects
-  auto object1 = new SceneObject("cube");
-  auto object2 = new SceneObject("cube");
-  object2->setPosition(glm::vec3(3, 2, 2));
+  auto object1 = new SceneObject("coaster");
 
   objects.emplace_back(object1);
-  objects.emplace_back(object2);
 
   // Ambient lights
   shader_structs::AmbientLight ambientLight(glm::vec3(1, 1, 1), 0.1f);
-  // ambientLights.emplace_back(ambientLight);
+  ambientLights.emplace_back(ambientLight);
 
   // Directional lights
-  shader_structs::DirectionalLight directionalLight1(glm::vec3(0, 1, 0), 1,
+  shader_structs::DirectionalLight directionalLight1(glm::vec3(1, 1, 1), 0.5,
                                                      glm::vec3(-1, -1, 1));
-  // directionalLights.emplace_back(directionalLight1);
+  directionalLights.emplace_back(directionalLight1);
 
   // Point lights
   shader_structs::PointLight pointLight1(glm::vec3(1, 1, 1), 1,
                                          glm::vec3(5, 5, 5), 1e-3);
-  pointLights.emplace_back(pointLight1);
+  // pointLights.emplace_back(pointLight1);
 }
