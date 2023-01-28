1f4eb8b6365a6a6ec759267406721ab4c5dfa5cc
Evan MASSENAVETTE <evan.massenavette@outlook.fr>
2023-01-28 20:35:41 +0100

    Use App in Controls::processInputs

--- src/controls.hpp
+++ src/controls.hpp
@@ -4,12 +4,26 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
+#include "app.hpp"
+#include "camera/camera.hpp"
+#include "camera/flying_camera.hpp"
+#include "camera/following_camera.hpp"
+
 #include <glm/glm.hpp>
 
+/**
+ * Handles controls of the application
+ */
 class Controls {
  public:
   // Constructor
   Controls(Camera::Type cameraType = Camera::Type::Flying);
+
+  /**
+   * Processes the inputs received
+   */
+  void processInputs(App& app);
+
   Camera& getCurrentCamera(FlyingCamera& flyingCamera,
                            FollowingCamera& followingCamera);
 
